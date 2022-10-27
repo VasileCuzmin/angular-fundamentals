12b2de2cb92bbab2ed3aa645000d0c048526bd01
Vasile Cuzmin <vasile.cuzmin@cegeka.com>
2022-10-27 12:24:23 +0300

    add user module

---
+++ src/app/user/user.routes.ts
@@ -0,0 +1,9 @@
+import { ProfileComponent } from "./profile.component";
+
+export const userRoutes = [
+    { path: 'profile', component: ProfileComponent }
+
+    /// not /profile
+    /// but
+    /// user/profile
+]
